7e9502e2fd68085cd8cf340b16a1c08fc352e9ef
Prithwijit98 <36706697+Prithwijit98@users.noreply.github.com>
2020-09-13 18:16:17 +0530

    Add files via upload

---
+++ assets/js/extra.js
@@ -0,0 +1,18 @@
+/*var smodal = document.getElementById("signinform");
+
+
+
+
+function sform()
+{
+	smodal.style.display="block";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == smodal) {
+    smodal.style.display = "none";
+  }
+ 
+}
+*/
